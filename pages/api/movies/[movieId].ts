f8da4c4fe1650d847c5d1a100491dccf941861d2
monkepi18 <patelpiyush@gmail.com>
2023-06-20 02:03:17 +0530

    Bugs fixed

--- pages/api/movies/[movieId].ts
+++ pages/api/movies/[movieId].ts
@@ -1,25 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
 import prismadb from '@/lib/prismadb';
 import serverAuth from "@/lib/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
     if (req.method !== 'GET') {
       return res.status(405).end();
     }
 
-    try {
-    const { currentUser } = await serverAuth(req, res);
+    await serverAuth(req, res);
 
-    const favoritedMovies = await prismadb.movie.findMany({
+    const { movieId } = req.query;
+
+    if (typeof movieId !== 'string') {
+      throw new Error('Invalid Id');
+    }
+
+    if (!movieId) {
+      throw new Error('Missing Id');
+    }
+
+    const movies = await prismadb.movie.findUnique({
       where: {
-        id: {
-          in: currentUser?.favoriteIds,
-        }
+        id: movieId
       }
     });
 
-    return res.status(200).json(favoritedMovies);
+    if(!movies){
+      throw new Error('Invalid Id');
+    }
+
+    return res.status(200).json(movies);
   } catch (error) {
     console.log(error);
     return res.status(500).end();
